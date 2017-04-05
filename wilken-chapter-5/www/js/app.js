22a77cd9b0ac796c5fde2519485ddd60662f0279
Tim Sellmyer <tjsellmyer1@buffs.wtamu.edu>
2017-04-05 06:08:50 +0000

    Final for 4-4-17

--- wilken-chapter-5/www/js/app.js
+++ wilken-chapter-5/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('App', ['ionic'])
+angular.module('App', ['ionic', 'highcharts-ng'])
 .config(function ($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('tabs', {
@@ -24,10 +24,11 @@ angular.module('App', ['ionic'])
       }
     })
     .state('tabs.history', {
-      url: '/history',
+      url: '/history?currency',
       views: {
         'history-tab': {
-          templateUrl: 'views/history/history.html'
+          templateUrl: 'views/history/history.html',
+          controller: 'HistoryController'
         }
       }
     })
@@ -51,30 +52,6 @@ angular.module('App', ['ionic'])
     $urlRouterProvider.otherwise('/tabs/rates');
 })
 
-.factory('Currencies', function () {
-  return [
-    { code: 'AUD', text: 'Australian Dollar', selected: true },
-    { code: 'BRL', text: 'Brazilian Real', selected: false },
-    { code: 'CAD', text: 'Canadian Dollar', selected: true },
-    { code: 'CHF', text: 'Swiss Franc', selected: false },
-    { code: 'CNY', text: 'Chinese Yuan', selected: true},
-    { code: 'EUR', text: 'Euro', selected: true },
-    { code: 'GBP', text: 'British Pound Sterling', selected: true },
-    { code: 'IDR', text: 'Indonesian Rupiah', selected: false },
-    { code: 'ILS', text: 'Israeli New Sheqel', selected: false },
-    { code: 'MXN', text: 'Mexican Peso', selected: true },
-    { code: 'NOK', text: 'Norwegian Krone', selected: false },
-    { code: 'NZD', text: 'New Zealand Dollar', selected: false },
-    { code: 'PLN', text: 'Polish Zloty', selected: false },
-    { code: 'RON', text: 'Romanian Leu', selected: false },
-    { code: 'RUB', text: 'Russian Ruble', selected: true },
-    { code: 'SEK', text: 'Swedish Krona', selected: false },
-    { code: 'SGD', text: 'Singapore Dollar', selected: false },
-    { code: 'USD', text: 'United States Dollar', selected: true },
-    { code: 'ZAR', text: 'South African Rand', selected: false }
-  ];
-});
-
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -91,63 +68,26 @@ angular.module('App', ['ionic'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
-
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
+.factory('Currencies', function () {
+  return [
+    { code: 'AUD', text: 'Australian Dollar', selected: true },
+    { code: 'BRL', text: 'Brazilian Real', selected: false },
+    { code: 'CAD', text: 'Canadian Dollar', selected: true },
+    //{ code: 'CHF', text: 'Swiss Franc', selected: false },
+    { code: 'CNY', text: 'Chinese Yuan', selected: true},
+    { code: 'EUR', text: 'Euro', selected: true },
+    { code: 'GBP', text: 'British Pound Sterling', selected: true },
+    { code: 'IDR', text: 'Indonesian Rupiah', selected: false },
+    { code: 'ILS', text: 'Israeli New Sheqel', selected: false },
+    { code: 'MXN', text: 'Mexican Peso', selected: true },
+    //{ code: 'NOK', text: 'Norwegian Krone', selected: false },
+    { code: 'NZD', text: 'New Zealand Dollar', selected: false },
+    { code: 'PLN', text: 'Polish Zloty', selected: false },
+    //{ code: 'RON', text: 'Romanian Leu', selected: false },
+    { code: 'RUB', text: 'Russian Ruble', selected: true },
+    { code: 'SEK', text: 'Swedish Krona', selected: false },
+    { code: 'SGD', text: 'Singapore Dollar', selected: false },
+    { code: 'USD', text: 'United States Dollar', selected: true },
+    { code: 'ZAR', text: 'South African Rand', selected: false }
+  ];
 });
