7a01ec03dbbb625eda9f6c1122ea798700411fad
Tim Sellmyer <tjsellmyer1@buffs.wtamu.edu>
2017-04-05 19:30:42 +0000

    update

--- wilken-chapter-5/www/views/rates/rates.js
+++ wilken-chapter-5/www/views/rates/rates.js
@@ -1,7 +1,31 @@
 angular.module('App')
-.controller('RatesController', function ($scope, $http, $ionicPopover, Currencies) {
+.controller('RatesController', function ($scope, $http, $ionicPopover, 
+  $interval, Currencies) {
   
   $scope.currencies = Currencies;
+  
+  $scope.theTime = new Date().toLocaleTimeString();
+  $interval(function () {
+    $scope.theTime = new Date().toLocaleTimeString();
+  }, 1000);
+  
+  $scope.$on('$destroy', function() {
+      $scope.stopTime();
+  });
+  
+  .directive('myCurrentTime', ['$interval', 'dateFilter',
+    function($interval, dateFilter) {
+      return function(scope, element, attrs) {
+        var format,
+            stopTime;
+            
+      function updateTime() {
+        element.text(dateFilter(new Date(), format));
+      }
+      
+      
+      }
+    }])
 
   $ionicPopover.fromTemplateUrl('views/rates/help-popover.html', {
     scope: $scope,
